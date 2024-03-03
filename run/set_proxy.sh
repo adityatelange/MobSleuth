5555e87a4f5b96405d07dcc10fa84ce09c7fed9c
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-03-03 18:24:21 +0530

    Add check for connected adb device in set_proxy.sh and unset_proxy.sh scripts

--- run/set_proxy.sh
+++ run/set_proxy.sh
@@ -2,4 +2,10 @@
 #
 # This script sets the proxy for the Android emulator.
 
+# check if adb devce is connected using adb devices command
+if [ -z "$(adb devices | grep -v List)" ]; then
+    echo -e "${GREEN}[+] No device connected${NC}"
+    exit 1
+fi
+
 adb shell settings put global http_proxy "$(ip -f inet addr show enp0s3 | awk '/inet/ {print $2}' | cut -d/ -f1):8080"
--- run/unset_proxy.sh
+++ run/unset_proxy.sh
@@ -2,4 +2,10 @@
 #
 # This script unsets the proxy for the Android emulator.
 
+# check if adb devce is connected using adb devices command
+if [ -z "$(adb devices | grep -v List)" ]; then
+    echo -e "${GREEN}[+] No device connected${NC}"
+    exit 1
+fi
+
 adb shell settings put global http_proxy :0

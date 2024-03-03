76a2c5ea8342125494fd9da7a196177f8b1a23de
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-03-03 17:25:38 +0530

    Add ~/.local/bin to PATH in $HOME/.bashrc

--- install/common_deps.sh
+++ install/common_deps.sh
@@ -14,4 +14,13 @@ sudo apt update -yqq
 echo -e "${GREEN}[+] Installing deps${NC}"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip -yqq --no-install-recommends
 
+# Check if $HOME.local/bin is in PATH and add it if not
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo -e "${GREEN}[+] Adding ~/.local/bin to PATH in $HOME/.bashrc${NC}"
+    echo "" >> $HOME/.bashrc
+    echo "# set PATH so it includes user's private bin if it exists" >> $HOME/.bashrc
+    echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.bashrc
+    source $HOME/.bashrc
+fi
+
 echo -e "${GREEN}[+] Done!${NC}"

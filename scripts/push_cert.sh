386ea881dbdcdb79515130cdcab89760c5019dd0
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-03-03 18:23:54 +0530

    Add check for connected adb device before pushing certificate

--- scripts/push_cert.sh
+++ scripts/push_cert.sh
@@ -12,6 +12,12 @@ openssl x509 -inform der -in certificate.der -out certificate.cer
 
 hashed_name=`openssl x509 -inform PEM -subject_hash_old -in certificate.cer | head -1` && cp certificate.cer $hashed_name.0
 
+# check if adb devce is connected using adb devices command
+if [ -z "$(adb devices | grep -v List)" ]; then
+    echo -e "${GREEN}[+] No device connected${NC}"
+    exit 1
+fi
+
 adb push $hashed_name.0 /system/etc/security/cacerts
 
 adb shell chmod 664 /system/etc/security/cacerts/$hashed_name.0

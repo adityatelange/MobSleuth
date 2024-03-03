1bc2fc2936218db072c76c032388df13d4b09820
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-03-03 17:38:44 +0530

    Update common_deps.sh to include python-is-python3 dependency

--- install/common_deps.sh
+++ install/common_deps.sh
@@ -12,7 +12,7 @@ echo -e "${GREEN}[#] COMMON DEPS${NC}"
 echo -e "${GREEN}[+] Updating package list${NC}"
 sudo apt update -yqq
 echo -e "${GREEN}[+] Installing deps${NC}"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip -yqq --no-install-recommends
+sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip python-is-python3 -yqq --no-install-recommends
 
 # Check if $HOME.local/bin is in PATH and add it if not
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then

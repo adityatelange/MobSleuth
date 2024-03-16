c1a260aecf9f2ad271c5035386eb73b16494d8ed
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2024-03-17 00:06:52 +0530

    Add more pkgs to clean from lubuntu

--- clean_lubuntu.sh
+++ clean_lubuntu.sh
@@ -11,7 +11,7 @@ WORKING_DIR=$HOME/MobSleuth
 
 
 echo -e "${GREEN}[+] Cleaning stuff...${NC}"
-sudo apt purge libreoffice-* quassel* transmission-* qpdfview* skanlite* 2048-qt* bluedevil* -y
+sudo apt purge libreoffice-* quassel* transmission-* qpdfview* skanlite* 2048-qt* bluedevil* vlc-* imagemagick* noblenote -y
 sudo apt autoremove -y
 sudo apt autoclean
 echo -e "${GREEN}[+] Done!${NC}"
